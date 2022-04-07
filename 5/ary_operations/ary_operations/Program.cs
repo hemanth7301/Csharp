713c50f6cfaef7afe71e64626979584ae2a3b549
hemanth7301 <saihemanthsakhamuri@gmail.com>
2022-04-07 19:31:07 +0530

    fifth

---
+++ 5/ary_operations/ary_operations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ary_operations
+{
+    class Program
+    {
+        static void print(int[] temp) {
+            foreach (int i in temp)
+            {
+                Console.WriteLine(i + " ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter array size");
+            int size;
+            int.TryParse(Console.ReadLine(),out size);
+
+            int[] arr = new int[size];
+            Console.WriteLine("Enter array elements");
+            for (int i = 0; i < size; i++) {
+                int.TryParse(Console.ReadLine(), out arr[i]);
+            }
+
+            Console.WriteLine("original array: ");
+            print(arr);
+
+            
+            int[] temp_arr=new int[size];
+
+            Array.Copy(arr,temp_arr,size);
+            Console.WriteLine("Copied array: ");
+            print(temp_arr);
+
+            Array.Sort(temp_arr);
+            Console.WriteLine("Sorted array: ");
+            print(temp_arr);
+
+            Array.Reverse(temp_arr);
+            Console.WriteLine("Reversed array: ");
+            print(temp_arr);
+
+            Array.Clear(temp_arr,0,size);
+            Console.WriteLine("Cleared array: ");
+            print(temp_arr);
+        }
+    }
+}
---
+++ 5/list/list/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = new List<string>();
+            string c = "yes";
+            while (c == "yes") {
+                Console.WriteLine("Enter employee name ");
+                employees.Add(Console.ReadLine());
+                Console.WriteLine("Do you want to add more yes/no");
+                c = Console.ReadLine();
+            }
+            Console.WriteLine("Employee List");
+            foreach (string i in employees) {
+                Console.WriteLine(i + " ");
+            }
+            
+
+            Console.WriteLine("No of employees: " + employees.Count);
+
+        }
+    }
+}

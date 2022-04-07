d8235aea39d52c7634f79f6b3edd5fb51e0804c1
hemanth7301 <saihemanthsakhamuri@gmail.com>
2022-04-07 19:34:44 +0530

    fifth

--- 6/delegates/icicibank/Program.cs
+++ 6/delegates/icicibank/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace icicibank
+namespace bank
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
